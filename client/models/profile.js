311c4b9aec23a25e5f606a73bf509edbfcda9a69
Jon Edwards <jkedwards@me.com>
2015-05-15 23:24:10 -0700

    changes to index.less

---
+++ client/models/profile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('recruitr')
+.factory('Profile', function($rootScope, $http, nodeUrl){
+  function Profile(){
+  }
+  Profile.find = function(){
+    return $http.get(nodeUrl + '/profiles');
+  };
+  Profile.save = function(student){
+    return $http.post(nodeUrl + '/profiles', student);
+  };
+});
--- client/views/profiles/profiles-new.js
+++ client/views/profiles/profiles-new.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('recruitr')
-.controller('ProfilesNewCtrl', function(){
+.controller('ProfilesNewCtrl', function($scope, Profile, $state){
+  $scope.create = function(student){
+    Profile.save(student)
+    .then(function(){
+      $state.go('profiles.list');
+    });
+  };
   console.log('in ProfilesNewCtrl');
 });
