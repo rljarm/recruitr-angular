f676d20f3b745a8ce50e803ec7a7f43ecaaf9598
Jon Edwards <jkedwards@me.com>
2015-05-16 16:54:13 -0700

    update users new page, user data, and nav to include fake bearer token

--- client/views/users/users-new.js
+++ client/views/users/users-new.js
@@ -1,15 +1,21 @@
 'use strict';
 
 angular.module('recruitr')
-.controller('UsersNewCtrl', function($scope, User){
+.controller('UsersNewCtrl', function($scope, User, $state, $window){
   console.log('in UsersNewCtrl');
 
   $scope.create = function(obj){
-  	var user = new User(obj);
-	user.save(obj)
-    .then(function(){
-      $state.go('users.list');
-    });
+    // var user = new User(obj);
+    if($scope.user.password1 === $scope.user.password2){
+      obj.password = $scope.user.password1;
+      delete obj.password1;
+      delete obj.password2;
+      User.save(obj)
+      .then(function(){
+        $state.go('users.list');
+      });
+    }else{
+      $window.swal({title: 'Password Error', text: 'Passwords do not match. Please try again.', type: 'error'});
+    }
   };
-
 });
