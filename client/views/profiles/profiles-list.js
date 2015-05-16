26c3aa9b112f391a983626ef57a1a2d536ecfb52
Dick <rljarm@gmail.com>
2015-05-16 14:13:21 -0700

    full pagination functionality added

--- client/views/profiles/profiles-list.js
+++ client/views/profiles/profiles-list.js
@@ -46,6 +46,27 @@ angular.module('recruitr')
   github: 'http://github.com'
   }
 ];
+  $scope.page = 1;
+  $scope.moveBack = false;
+  $scope.moveForward = true;
+  $scope.changePage = function(change){
+  if(change === 'next' && $scope.users.length === 10){
+    $scope.page += 1;
+    $scope.moveBack = true;
+  } else if(change === 'prev' && $scope.page !== 1){
+    $scope.page -= 1;
+    $scope.moveForward = true;
+  }
+  Profile.find($scope.page)
+  .then(function(response){
+    $scope.users = response.data.users;
+  });
+  if($scope.page === 1){
+    $scope.moveBack = false;
+  } else if($scope.users.length < 10){
+    $scope.moveForward = false;
+  }
+};
   Profile.find()
   .then(function(response){
     $scope.students = response.data.students;
