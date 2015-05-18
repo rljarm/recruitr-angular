3684777a9a31ec1ea2d2a8cdb4e364ad3823957c
Jon Edwards <jkedwards@me.com>
2015-05-17 19:20:53 -0700

    fixes to users-new.js

--- client/views/users/users-new.js
+++ client/views/users/users-new.js
@@ -4,7 +4,6 @@ angular.module('recruitr')
 .controller('UsersNewCtrl', function($scope, User, $state, $window){
   console.log('in UsersNewCtrl');
   $scope.isEdit = false;
-  checkState();
   console.log($state.params);
 
   function checkState(){
@@ -15,18 +14,21 @@ angular.module('recruitr')
         $scope.user = response.data;
       });
     }
-  };
+  }
+
+  checkState();
+
   $scope.edit = function(obj){
     console.log('before copy', obj._id);
     var o = angular.copy(obj);
     console.log('copy ID', o._id);
-    delete o.__v
+    delete o.__v;
     delete o.$$hashkey;
     delete o.createdAt;
     delete o.password2;
     o.password = o.password1;
     delete o.password1;
-    delete o._id
+    delete o._id;
     User.edit(o, obj._id)
     .then(function(){
       $state.go('users.list');
