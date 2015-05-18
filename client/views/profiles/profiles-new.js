3de5fb89f7c3572ae72efda1d42ef0b20d519b73
Dick <rljarm@gmail.com>
2015-05-17 20:04:35 -0700

    added new profile adds

--- client/views/profiles/profiles-new.js
+++ client/views/profiles/profiles-new.js
@@ -3,31 +3,52 @@
 angular.module('recruitr')
 .controller('ProfilesNewCtrl', function($scope, Profile, $state, msaList){
   $scope.msa = msaList;
+  var forHire;
+  var remote;
+  var relocate;
 
   $scope.create = function(){
+    console.log('loc', $scope.student.preferedLocation1);
+    if($scope.student.forHire === undefined){
+      forHire = false;
+    }else{
+      forHire = true;
+    }
+    if($scope.student.remoteWorking === undefined){
+      remote = false;
+    }else{
+      remote = true;
+    }
+    if($scope.student.relocation === undefined){
+      relocate = false;
+    }else{
+      relocate = true;
+    }
+    var skills = $scope.student.skills.split(', ');
+    console.log('skills', skills);
     var student = {
         firstName: $scope.student.firstName,
         lastName: $scope.student.lastName,
         photo: $scope.student.photo,
-        skills: $scope.student.skills,
+        skills: skills,
         exposure: $scope.student.exposure,
         bio: $scope.student.blurb,
         location: $scope.student.location,
         interests: [
-          $scope.student.interest1,
-          $scope.student.interest2,
-          $scope.student.interest3,
-          $scope.student.interest4,
-          $scope.student.interest5
+          $scope.student.interests.interest1,
+          $scope.student.interests.interest2,
+          $scope.student.interests.interest3,
+          $scope.student.interests.interest4,
+          $scope.student.interests.interest5
         ],
         locationPref: [
-          $scope.student.preferedLocation1,
-          $scope.student.preferedLocation2,
-          $scope.student.preferedLocation3
+          $scope.student.preferedLocation.preferedLocation1,
+          $scope.student.preferedLocation.preferedLocation2,
+          $scope.student.preferedLocation.preferedLocation3
         ],
-        remote: $scope.student.remoteWorking,
-        relocate: $scope.student.relocation,
-        available: $scope.student.forHire,
+        remote: remote,
+        relocate: relocate,
+        available: forHire,
         education: $scope.student.education,
         contact: {
           email: $scope.student.email,
@@ -36,10 +57,12 @@ angular.module('recruitr')
         social: {
           github: $scope.student.github,
           twitter: $scope.student.twitter,
-          linkedin: $scope.student.linkedin,
+          linkedIn: $scope.student.linkedin,
           stackoverflow: $scope.student.stackoverflow
         }
       };
+    console.log(student);
+    console.log('$scope.student', $scope.student);
     Profile.save(student)
     .then(function(){
       $state.go('profiles.list');
